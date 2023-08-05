48bfc09c6dcf6f8ce0bb96ab9177c196ba0f5e41
Moyseys <moyseys@gmail.com>
2023-08-05 18:35:37 -0300

    change server port

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 import app from './app';
 
-const port = process.env.APP_PORT;
+const port = process.env.PORT || process.env.APP_PORT;
 app.listen(port, () => { console.log(`Server is running -> http://localhost:${port}`); });
